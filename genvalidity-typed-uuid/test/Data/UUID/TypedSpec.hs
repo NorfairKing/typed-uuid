673fb60e059e2ff9598f671c24c01305c383b52b
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-04-12 22:56:39 +0200

    Updated the tests to be more accurate after genvalidity-0.11

--- genvalidity-typed-uuid/test/Data/UUID/TypedSpec.hs
+++ genvalidity-typed-uuid/test/Data/UUID/TypedSpec.hs
@@ -1,18 +1,18 @@
 {-# LANGUAGE TypeApplications #-}
 
 module Data.UUID.TypedSpec
-    ( spec
-    ) where
+  ( spec,
+  )
+where
 
+import Data.GenValidity.UUID.Typed ()
+import Data.UUID.Typed
 import Test.Hspec
 import Test.Validity
 import Test.Validity.Aeson
 
-import Data.GenValidity.UUID.Typed ()
-import Data.UUID.Typed
-
 spec :: Spec
 spec = do
-    eqSpec @(UUID Int)
-    genValidSpec @(UUID Int)
-    jsonSpec @(UUID Int)
+  eqSpecOnValid @(UUID Int)
+  genValidSpec @(UUID Int)
+  jsonSpecOnValid @(UUID Int)
