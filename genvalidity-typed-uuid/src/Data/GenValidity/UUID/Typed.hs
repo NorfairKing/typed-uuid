5415eaeee9817dfc4846fe4d73efce9312281b27
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-04-14 20:12:42 +0200

    Upgraded validity and the generator for typed uuids

--- genvalidity-typed-uuid/src/Data/GenValidity/UUID/Typed.hs
+++ genvalidity-typed-uuid/src/Data/GenValidity/UUID/Typed.hs
@@ -7,9 +7,13 @@ import Data.GenValidity.UUID ()
 import Data.UUID.Typed
 import Test.QuickCheck
 
-instance GenUnchecked (UUID a)
+instance GenUnchecked (UUID a) where
+  genUnchecked = UUID <$> genUnchecked
+  shrinkUnchecked (UUID u) = UUID <$> shrinkUnchecked u
 
-instance GenValid (UUID a)
+instance GenValid (UUID a) where
+  genValid = UUID <$> genValid
+  shrinkValid (UUID u) = UUID <$> shrinkValid u
 
 instance Arbitrary (UUID a) where
-    arbitrary = genValid
+  arbitrary = genValid
