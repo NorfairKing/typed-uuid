dc5255bd162d92806827803bd5d0e3c9eb4b4e2e
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-11-20 19:17:24 +0100

    removed a rogue file

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -8,7 +8,6 @@ import sources.nixpkgs {
     (import (sources.validity + "/nix/overlay.nix"))
     (import (sources.autodocodec + "/nix/overlay.nix"))
     (final: previous: { inherit (import sources.gitignore { inherit (final) lib; }) gitignoreSource; })
-    (import ./gitignore-src.nix)
     (import ./overlay.nix)
   ];
   config.allowUnfree = true;
